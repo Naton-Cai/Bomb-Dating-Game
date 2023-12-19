9eb3542caaa0dc832c6635d2cd5227271ef3ec27
Naton-Cai <natoncai@gmail.com>
2023-12-19 13:49:29 -0800

    fixed the audio player object for general use

--- SFX/audio.gd
+++ SFX/audio.gd
@@ -3,13 +3,13 @@ var end_timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	end_timer = self.get_parent().get_node("Main/Dating_Scene/Game_Fuse_Timer")
+	#end_timer = self.get_root().get_node("Main/Dating_Scene/Game_Fuse_Timer")
 	print(end_timer)
-	end_timer.timeout.connect(self._on_game_fuse__timer_timeout)
+	#end_timer.timeout.connect(self._on_game_fuse__timer_timeout)
 	self.play()
 	await self.finished
 	
 	
 
-func _on_game_fuse__timer_timeout():
-	self.queue_free()
+#func _on_game_fuse__timer_timeout():
+	#self.queue_free()
