b29537238e8b9276e79c93264dc021079ce3531e
NhojDev <johnduypham@gmail.com>
2023-12-20 13:55:21 -0800

    GoodEndingScene

---
+++ Scene/End Screen/Sprite2D.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+@onready var animation = $AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation.play("Hearts")
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
