f0a155ac0094da50d574657cb6f0b5b4f82be8de
Naton-Cai <natoncai@gmail.com>
2023-12-20 13:28:52 -0800

    added a few sfx

--- UI/Health_bar/Health_Bar.gd
+++ UI/Health_bar/Health_Bar.gd
@@ -1,7 +1,10 @@
 extends TextureProgressBar
 var tween
+var sound
 @onready var sfx = preload("res://SFX/audio.tscn")
 @onready var audio_music = preload("res://SFX/Bomb Burning.wav")
+@onready var audio_healthup = preload("res://SFX/healthup.wav")
+@onready var audio_healthdown = preload("res://SFX/healthdown.wav")
 
 
 func _ready():
@@ -9,6 +12,7 @@ func _ready():
 
 func set_health(new_health, change):
 	var music
+
 	if not is_instance_valid(music):
 		music = sfx.instantiate() as AudioStreamPlayer2D
 		var audio = audio_music
@@ -24,8 +28,20 @@ func set_health(new_health, change):
 	tween.finished.connect(self._on_tween_finish)
 	tween.tween_property(self, "value", new_health, 1).set_trans(Tween.TRANS_LINEAR)
 	if change < -4:
+			sound = sfx.instantiate() as AudioStreamPlayer2D
+			var audio = audio_healthup
+			sound.stream = audio
+			sound.global_position = self.global_position
+			sound.volume_db = -5
+			self.add_sibling(sound)  
 			self.tint_progress = Color("#00a100", 1)
 	if change > 5:
+			sound = sfx.instantiate() as AudioStreamPlayer2D
+			var audio = audio_healthdown
+			sound.stream = audio
+			sound.global_position = self.global_position
+			#sound.volume_db = 
+			self.add_sibling(sound)  
 			self.tint_progress = Color("#fd0037", 1)
 	
 	#print(change)
